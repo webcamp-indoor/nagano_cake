97edbe36b7be0b34ed28f7e188a923563ec1f867
EC2 Default User <ec2-user@ip-172-31-15-49.ec2.internal>
2022-06-15 07:45:35 +0000

    [Add]ルーティングを追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,50 +1,55 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => "homes#about"
+
+    resource :customers, only:[:show, :edit, :update] do
+      get "confirm" => "customers#confirm"
+      patch "withdrawal" => "customers#withdrawal"
+    end
+
+    resources :address, only:[:index, :create, :destroy, :edit, :update]
+
+    resources :items, only:[:index, :show]
+
+    resources :cart_items, only:[:index, :create, :update, :destroy] do
+      collection do
+        delete :destroy_all
+      end
+    end
+
+    resources :orders, only:[:new, :create, :index, :show] do
+      collection do
+        post :confirm
+        get :complete
+      end
+    end
+  end
+
   namespace :admin do
-    get 'orders/show'
+    get "/" => "homes#top"
+
+    resources :customers, only:[:index, :show, :edit, :update]
+
+    resources :items, only:[:new, :create, :index, :show, :edit, :update]
+
+    resources :genres, only:[:index, :create, :edit, :update]
+
+    resources :orders, only:[:show, :update]
+
+    resources :order_details, only:[:update]
   end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/new'
-    get 'items/index'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  devise_for :admins
-  devise_for :customers
+
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
