9e5c3ee152ddf4713be9547df6b521cdcad1d99c
EC2 Default User <ec2-user@ip-172-31-15-49.ec2.internal>
2022-06-16 02:51:31 +0000

    [Add]admin側itemの実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,38 @@
 class Admin::ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
+
   def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_items_path
   end
 
   def index
+    @items = Item.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  def update
+    @item.update(item_params)
+    redirect_to admin_item_path(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
