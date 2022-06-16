0f92a7f94d9d351eb295eac253be198ccef71bdb
ayano309 <ayatomato.k@gmail.com>
2022-06-16 09:17:00 +0000

    [add]customerコントローラーに退会するアクション追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -22,7 +22,11 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdrawal
-
+    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行しました"
+    redirect_to root_path
   end
 
 private
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -33,4 +33,6 @@ class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :addresses, dependent: :destroy
+  
+  
 end
