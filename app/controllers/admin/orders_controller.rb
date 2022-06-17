97f15791317a13f6f1d30da698f237a662db272b
EC2 Default User <ec2-user@ip-172-31-15-49.ec2.internal>
2022-06-17 06:46:01 +0000

    [Add]adminの注文履歴一覧画面表示実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,14 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
+  end
+  
+  def update
+  
+  end
+  
+  def customer_index
+    
   end
 end
