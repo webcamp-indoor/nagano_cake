fdd8e1ef170628bb01aeed20bf4fe5aa9e94f614
EC2 Default User <ec2-user@ip-172-31-15-49.ec2.internal>
2022-06-18 05:25:57 +0000

    [Add]adminの顧客注文履歴画面表示

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -20,6 +20,11 @@ class Admin::CustomersController < ApplicationController
     end
   end
 
+  def order_index
+    @customer = Customer.find(params[:id])
+    @orders = Order.where(customer_id: @customer.id).page(params[:page]).per(10)
+  end
+
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email, :is_deleted)
