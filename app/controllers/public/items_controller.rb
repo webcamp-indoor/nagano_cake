9a064adce70a7c8a4db9a33f868be112295abc22
EC2 Default User <ec2-user@ip-172-31-15-49.ec2.internal>
2022-06-20 01:23:21 +0000

    [Update]トップページの商品一覧を調整

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,6 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(6)
+    @items = Item.page(params[:page]).per(8)
     @genres = Genre.all
   end
 
@@ -14,11 +14,12 @@ class Public::ItemsController < ApplicationController
     @genres = Genre.all
     params_genre = params[:id]
     items = Item.genre_search(params[:genre_id])
-    @items = items.page(params[:page]).per(6)
+    @items = items.page(params[:page]).per(8)
     @genre_name = Genre.find(params[:genre_id]).name
   end
 
   def word_search
+    @genres = Genre.all
     @items_search = Item.search(params[:keyword]).page(params[:page]).per(8)
   end
 end
